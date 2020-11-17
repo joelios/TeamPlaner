cb7acdbf07e6f0a996eef474711fdade717fb16a
Joelios <joel.mesmer@libracore.com>
2020-11-17 17:01:04 +0100

    #14

---
+++ teamplaner/www/statistiken.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, msmr.ch and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+
+no_cache = 1
+
+def get_context(context):
+	if frappe.session.user=='Guest':
+		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
