790bf5287c069787b72d2f32d7f9fbfe60bfea6b
Joelios <joel.mesmer@libracore.com>
2020-12-04 11:23:15 +0100

    added 'komme pünktlich' function to ereigniss

--- teamplaner/public/js/teamplaner.js
+++ teamplaner/public/js/teamplaner.js
@@ -24,6 +24,11 @@ function komme_zu_spaet(event_name) {
 	change_status('teamplaner.www.ereignisse.komme_zu_spaet', event_name);
 }
 
+function komme_puenktlich(event_name) {
+	show_waiting_screen();
+	change_status('teamplaner.www.ereignisse.anmelden', event_name);
+}
+
 function change_status(_method, _event_name) {
 	console.log(_method);
 	console.log(_event_name);
