2faea9da200236d06232863fa808e45b5d330570
Joelios <joel.mesmer@libracore.com>
2021-02-22 18:16:21 +0100

    added next event functionality on dashboard

--- teamplaner/www/dashboard.py
+++ teamplaner/www/dashboard.py
@@ -11,3 +11,8 @@ no_cache = 1
 def get_context(context):
 	if frappe.session.user=='Guest':
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
+	context['next_event'] = {}
+	all_events = frappe.db.sql("""SELECT `name` FROM `tabTP Event` ORDER BY `event_date` ASC LIMIT 1""", as_list=True)
+	if len(all_events) == 1:
+		context['next_event'] = frappe.get_doc("TP Event", all_events[0][0])
+	return context
