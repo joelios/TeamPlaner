3c89f46b2b97808c737e04d2587eac2360e8bcaa
Joelios <joel.mesmer@libracore.com>
2021-02-22 18:25:05 +0100

    added next game functionality to dashboard

--- teamplaner/www/dashboard.py
+++ teamplaner/www/dashboard.py
@@ -12,7 +12,11 @@ def get_context(context):
 	if frappe.session.user=='Guest':
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
 	context['next_event'] = {}
-	all_events = frappe.db.sql("""SELECT `name` FROM `tabTP Event` ORDER BY `event_date` ASC LIMIT 1""", as_list=True)
-	if len(all_events) == 1:
-		context['next_event'] = frappe.get_doc("TP Event", all_events[0][0])
+	next_event = frappe.db.sql("""SELECT `name` FROM `tabTP Event` WHERE `typ` != 'Match' ORDER BY `event_date` ASC LIMIT 1""", as_list=True)
+	if len(next_event) == 1:
+		context['next_event'] = frappe.get_doc("TP Event", next_event[0][0])
+	context['next_game'] = {}
+	next_game = frappe.db.sql("""SELECT `name` FROM `tabTP Event` WHERE `typ` = 'Match' ORDER BY `event_date` ASC LIMIT 1""", as_list=True)
+	if len(next_game) == 1:
+		context['next_game'] = frappe.get_doc("TP Event", next_game[0][0])
 	return context
