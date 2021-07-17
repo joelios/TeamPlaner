7fcf22d499f6779886d44381db7af81bb2bafaf3
Joelios <joel.mesmer@libracore.com>
2021-07-17 22:26:49 +0200

    swissunihockey

--- teamplaner/www/dashboard.py
+++ teamplaner/www/dashboard.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from teamplaner.www.statistiken import get_top_three
+from teamplaner.www.swissunihockey import get_tabelle
 
 no_cache = 1
 
@@ -21,4 +22,63 @@ def get_context(context):
 	if len(next_game) == 1:
 		context['next_game'] = frappe.get_doc("TP Event", next_game[0][0])
 	context['top_three'] = get_top_three()
+	context['tabelle'] = _get_tabelle()
 	return context
+
+def _get_tabelle():
+	tabelle = get_tabelle('short')
+	data = {}
+	data['rankings'] = []
+	data['go_for_it'] = False
+	if len(tabelle['rankings']) > 0:
+		data['go_for_it'] = True
+		counter = 0
+		for ranking in tabelle['rankings']:
+			if ranking['cells'][1] == 'HC Rychenberg Winterthur II':
+				if counter > 0:
+					# hcr = 2. oder schlechter
+					data['rankings'] = [
+						[
+							tabelle['regions'][0]['rows'][counter - 1]['cells'][0],
+							tabelle['regions'][0]['rows'][counter - 1]['cells'][1],
+							tabelle['regions'][0]['rows'][counter - 1]['cells'][3],
+							tabelle['regions'][0]['rows'][counter - 1]['cells'][4]
+						],
+						[
+							'<b>' + ranking['cells'][0] + '</b>',
+							'<b>' + ranking['cells'][1] + '</b>',
+							'<b>' + ranking['cells'][3] + '</b>',
+							'<b>' + ranking['cells'][4] + '</b>'
+						],
+						[
+							tabelle['regions'][0]['rows'][counter + 1]['cells'][0],
+							tabelle['regions'][0]['rows'][counter + 1]['cells'][1],
+							tabelle['regions'][0]['rows'][counter + 1]['cells'][3],
+							tabelle['regions'][0]['rows'][counter + 1]['cells'][4]
+						]
+					]
+				else:
+					# hcr 1.
+					data['rankings'] = [
+						[
+							'<b>' + ranking['cells'][0] + '</b>',
+							'<b>' + ranking['cells'][1] + '</b>',
+							'<b>' + ranking['cells'][3] + '</b>',
+							'<b>' + ranking['cells'][4] + '</b>'
+						],
+						[
+							tabelle['regions'][0]['rows'][counter + 1]['cells'][0],
+							tabelle['regions'][0]['rows'][counter + 1]['cells'][1],
+							tabelle['regions'][0]['rows'][counter + 1]['cells'][3],
+							tabelle['regions'][0]['rows'][counter + 1]['cells'][4]
+						],
+						[
+							tabelle['regions'][0]['rows'][counter + 2]['cells'][0],
+							tabelle['regions'][0]['rows'][counter + 2]['cells'][1],
+							tabelle['regions'][0]['rows'][counter + 2]['cells'][3],
+							tabelle['regions'][0]['rows'][counter + 2]['cells'][4]
+						]
+					]
+			else:
+				counter += 1
+	return data
--- teamplaner/www/swissunihockey.py
+++ teamplaner/www/swissunihockey.py
@@ -5,9 +5,34 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
+import requests
 
 no_cache = 1
 
 def get_context(context):
 	if frappe.session.user=='Guest':
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
+	context['spielplan'] = get_spielplan()
+	context['tabelle'] = get_tabelle('full')
+	return context
+
+def get_spielplan():
+	response = requests.get("https://api-v2.swissunihockey.ch/api/games?mode=team&team_id=428691&season=2021")
+	r = response.json()['data']
+	data = {}
+	data['titel'] = r['title']
+	data['games'] = []
+	for game in r['regions'][0]['rows']:
+		data['games'].append(game)
+	return data
+	
+def get_tabelle(view='full'):
+	response = requests.get("https://api-v2.swissunihockey.ch/api/rankings?season=2021&league=5&game_class=11&group=407867&view={view}".format(view=view))
+	r = response.json()['data']
+	data = {}
+	data['titel'] = r['title']
+	data['rankings'] = []
+	if len(r['regions']) > 0:
+		for ranking in r['regions'][0]['rows']:
+			data['rankings'].append(ranking)
+	return data
