c0dfcb13e24db31beea80af4ec093584f2b29fbf
Joelios <joel.mesmer@libracore.com>
2021-07-28 17:23:33 +0200

    #16

--- teamplaner/www/einstellungen.py
+++ teamplaner/www/einstellungen.py
@@ -5,9 +5,13 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
+from teamplaner.utils import get_user_profile
 
 no_cache = 1
 
 def get_context(context):
 	if frappe.session.user=='Guest':
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
+		
+	context['user_stamm'] = get_user_profile()
+	return context
